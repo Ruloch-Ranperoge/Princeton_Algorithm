2c0c867c300d8acf4846b43618db9b48b78d9b1e
LIU QINGHAPO <lqh996519@gmail.com>
2020-03-08 17:29:48 +0900

    Complete basic test

--- Algorithm_Assign4_8puzzle/Board.java
+++ Algorithm_Assign4_8puzzle/Board.java
@@ -1,36 +1,171 @@
-public class Board {
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
+public class Board {
+    private int[][] mat;
+    private int n;
+    private int hammingDis;
+    private int manhattanDis;
+    private int zeroIdxX;
+    private int zeroIdxY;
     // create a board from an n-by-n array of tiles,
     // where tiles[row][col] = tile at (row, col)
-    public Board(int[][] tiles){}
+    public Board(int[][] tiles){
+        n = tiles.length;
+        if(n < 2){
+            throw new IllegalArgumentException("Array size should be greater than 1");
+        }
+        if(n != tiles[0].length){
+            throw new IllegalArgumentException("Row should be equal to column.");
+        }
+        mat = new int[n][n];
+        for(int i = 0;i<n;i++)
+            System.arraycopy(tiles[i],0,mat[i],0,tiles[i].length);
+
+        hammingDis=0;
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<n;j++){
+                if (mat[i][j]!=0 && i*n+j+1 != mat[i][j])
+                    hammingDis ++;
+            }
+        }
+
+        int tmpTarX,tmpTarY;
+        manhattanDis = 0;
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<n;j++){
+                if(mat[i][j]==0)continue;
+                tmpTarX = (mat[i][j]-1)/n;
+                tmpTarY = (mat[i][j]-1)%n;
+                manhattanDis += Math.abs(tmpTarX-i) + Math.abs(tmpTarY-j);
+            }
+        }
+
+        zeroIdxX=0;
+        zeroIdxY=0;
+        for(int i=0;i<n;i++)
+            for(int j=0;j<n;j++)
+                if(mat[i][j]==0){
+                    zeroIdxX = i;
+                    zeroIdxY = j;
+                }
+    }
 
     // string representation of this board
-    public String toString(){return "";}
+    public String toString(){
+        StringBuilder expBuffer = new StringBuilder();
+        expBuffer.append(n).append("\n");
+        for (int i = 0;i<n;i++){
+            for(int j=0;j<n;j++)
+                expBuffer.append(" ").append(mat[i][j]);
+            expBuffer.append("\n");
+        }
+
+        return expBuffer.toString();
+    }
 
     // board dimension n
-    public int dimension(){return 0;}
+    public int dimension(){
+        return n;
+    }
 
     // number of tiles out of place
-    public int hamming(){return 0;}
+    public int hamming(){
+        return hammingDis;
+    }
 
     // sum of Manhattan distances between tiles and goal
-    public int manhattan(){return 0;}
+    public int manhattan(){
+        return manhattanDis;
+    }
 
     // is this board the goal board?
-    public boolean isGoal(){return false;}
+    public boolean isGoal(){
+        for(int i=0;i<n;i++)
+            for(int j=0;j<n;j++)
+                if(mat[i][j]!= 0 && i*n+j+1 != mat[i][j])
+                    return false;
+        return true;
+    }
 
     // does this board equal y?
-    public boolean equals(Object y){return false;}
+    public boolean equals(Object y){
+        Board tmpY;
+        if(y instanceof Board)
+            tmpY = (Board)y;
+        else
+            return false;
+        if (n!=tmpY.dimension())
+            return false;
+        for(int i=0;i<n;i++)
+            for(int j=0;j<n;j++)
+                if(mat[i][j]!=tmpY.mat[i][j])
+                    return false;
+        return true;
+    }
 
+    private boolean inBound(int x,int y){
+        return x < n && x >= 0 && y < n && y >= 0;
+    }
+    private void exchangeElement(int x1, int y1, int x2, int y2){
+        int tmp = mat[x1][y1];
+        mat[x1][y1] = mat[x2][y2];
+        mat[x2][y2] = tmp;
+    }
     // all neighboring boards
-    public Iterable<Board> neighbors(){return null;}
+    public Iterable<Board> neighbors(){
+        List<Board> neighborBoards = new ArrayList<>();
+        int tmpX,tmpY;
+        int [] tmpXs = {zeroIdxX - 1, zeroIdxX, zeroIdxX + 1, zeroIdxX};
+        int [] tmpYs = {zeroIdxY, zeroIdxY + 1, zeroIdxY, zeroIdxY - 1};
+        for(int i = 0;i<tmpXs.length;i++){
+            tmpX = tmpXs[i];
+            tmpY = tmpYs[i];
+            if(inBound(tmpX,tmpY)){
+                exchangeElement(zeroIdxX,zeroIdxY,tmpX,tmpY);
+                neighborBoards.add(new Board(mat));
+                exchangeElement(zeroIdxX,zeroIdxY,tmpX,tmpY);
+            }
+        }
+        return neighborBoards;
+    }
 
     // a board that is obtained by exchanging any pair of tiles
-    public Board twin(){return null;}
+    public Board twin(){
+        int x1=0,y1=0;
+        int x2=0,y2=0;
+        int cnt = 0;
+        int x,y;
+        for(int i=0;i<n*n;i++){
+           x = i/n;
+           y = i%n;
+           if(cnt == 0 && mat[x][y]!=0){x1=x;y1=y;cnt=1;continue;}
+           else if(cnt == 1 && mat[x][y]!=0){x2=x;y2=y;cnt=2;continue;}
+           else if(cnt == 2)break;
+        }
+        exchangeElement(x1,y1,x2,y2);
+        Board tmp = new Board(mat);
+        exchangeElement(x1,y1,x2,y2);
+        return tmp;
+    }
 
     // unit testing (not graded)
     public static void main(String[] args){
-        System.out.println("Board main");
+        System.out.println("Board Unitest");
+        int [][] testMat = {{1,0,3},{4,2,5},{7,8,6}};
+        int [][] testMat2 = {{1,2,3},{4,5,6},{7,8,0}};
+        Board board1 = new Board(testMat);
+        Board board2 = new Board(testMat2);
+        System.out.println("Dimension:" + board1.dimension());
+        System.out.println("Expression:\n" + board1.toString());
+        System.out.println("Hamming:" + board1.hamming());
+        System.out.println("Manhattan:"+ board1.manhattan());
+        System.out.println("Board1 IsGoal:"+board1.isGoal());
+        System.out.println("Board2 IsGoal:"+board2.isGoal());
+        System.out.println("Board1 == Board2:"+board1.equals(board2));
+        System.out.println("Board1 twin:\n"+board1.twin().toString());
+
     }
 
 }
--- Algorithm_Assign4_8puzzle/Solver.java
+++ Algorithm_Assign4_8puzzle/Solver.java
@@ -1,16 +1,83 @@
-public class Solver {
+import edu.princeton.cs.algs4.MinPQ;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class Solver {
+    private boolean solvebale;
+    private int cnt;
+    private List<Board> sol;
     // find a solution to the initial board (using the A* algorithm)
-    public Solver(Board initial){}
+    public Solver(Board initial){
+        if (initial == null){
+            throw new IllegalArgumentException("initial board is null");
+        }
+        Comparator<Board> hammingComparator = new Comparator<Board>(){
+            @Override
+            public int compare(final Board b1, final Board b2){
+                return b1.hamming() - b2.hamming();
+            }
+        };
+        Comparator<Board> manhattanComparator = new Comparator<Board>(){
+            @Override
+            public int compare(final Board b1, final Board b2){
+                return b1.manhattan() - b2.manhattan();
+            }
+        };
+        MinPQ<Board> qMain = new MinPQ<>(manhattanComparator);
+        MinPQ<Board> qSub = new MinPQ<>(manhattanComparator);
+
+        qMain.insert(initial);
+        qSub.insert(initial.twin());
+        Board searchNodeMain;
+        Board searchNodeSub;
+        Board prevNodeMain = null;
+        Board prevNodeSub = null;
+        cnt = -1;
+        List<Board> solMain = new ArrayList<>();
+        List<Board> solSub = new ArrayList<>();
+        while(!qMain.isEmpty() && !qSub.isEmpty()){
+            searchNodeMain = qMain.delMin();
+            searchNodeSub = qSub.delMin();
+            solMain.add(searchNodeMain);
+            solSub.add(searchNodeSub);
+            //System.out.println(searchNodeMain.toString());
+            cnt ++;
+            if (searchNodeMain.isGoal()){
+                solvebale = true;
+                sol = solMain;
+                //System.out.println("Main solvable");
+                break;
+            }
+            if (searchNodeSub.isGoal()){
+                solvebale = false;
+                sol = solSub;
+                //System.out.println("Sub solvable");
+                break;
+            }
+            for(Board neigh:searchNodeMain.neighbors()){
+                if(neigh.equals(prevNodeMain))continue;
+                qMain.insert(neigh);
+            }
+            for(Board neigh:searchNodeSub.neighbors()){
+                if(neigh.equals(prevNodeSub))continue;
+                qSub.insert(neigh);
+            }
+            prevNodeMain = searchNodeMain;
+            prevNodeSub = searchNodeSub;
+        }
+
+    }
 
     // is the initial board solvable? (see below)
-    public boolean isSolvable(){return false;}
+    public boolean isSolvable(){return solvebale;}
 
     // min number of moves to solve initial board
-    public int moves(){return 0;}
+    public int moves(){return cnt;}
 
     // sequence of boards in a shortest solution
-    public Iterable<Board> solution(){return null;}
+    public Iterable<Board> solution(){return sol;}
 
     // test client (see below)
     public static void main(String[] args){
